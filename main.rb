31038366aa9e78cf870682b560543cf18964c3e2
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-25 19:30:02 -0300

    upgrade main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,24 @@
-main.rb
+require 'rest-client'
+require 'json'
+
+def menu
+	menu = "
+l*******************************l
+l           RubyTools           l
+l*******************************l
+l...............................l
+l    [ 01 ] Update Terminal     l
+l    [ 02 ] Upgrade Terminal    l
+l    [ 03 ] Clear Terminal      l
+l...............................l
+l          PythonTools          l
+l...............................l
+l    [ 05 ] Install Pynput      l
+l    [ 06 ] install Reqquests   l
+l    [ 07 ] Install Flask       l
+l    [ 08 ] Install Colorama    l
+l...............................l
+l            RubyTools          l
+l...............................l
+	"
+end
